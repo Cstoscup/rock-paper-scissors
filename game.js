ce041a0630f9694a54d42e8ef6decc266a8c39ff
Cstoscup <109521820+Cstoscup@users.noreply.github.com>
2022-09-02 18:28:16 -0400

    added rock, paper, scissors countdown and hesitation feature. started css

--- game.js
+++ game.js
@@ -1,3 +1,29 @@
+function displayGame() {
+    document.getElementById("play-game-button").className = "hidden";
+    document.getElementById("timer").className = "active";
+    let timeLeft = 3;
+    let timer = setInterval(function() {
+        if (timeLeft === 3) {
+            document.getElementById("timer").innerHTML = "Paper";
+        } else if (timeLeft === 2) {
+            document.getElementById("timer").innerHTML = "Scissors";
+        } else if (timeLeft === 1) {
+            document.getElementById("timer").innerHTML = "Shoot!";
+            clearInterval(timer);
+            document.getElementById("options").className = "active";
+        }
+        timeLeft = timeLeft - 1;
+    }, 1000);
+    setTimeout(function() {
+        document.getElementById("options").className = "hidden";
+        document.getElementById("round-score").className = "active";
+        document.querySelector("#choice-display").innerHTML = "Nothing!";
+        displayComputerChoice("...");
+    }, "5000");
+    // document.getElementById("game").className = "active";
+    // document.getElementById("play-game-button").className = "hidden";
+}
+
 function displayUserChoice(event) {
     event.preventDefault();
     document.querySelector("#choice-display").innerHTML = this.id;
@@ -19,7 +45,11 @@ function displayComputerChoice(userChoice) {
 }
 
 function decideWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
+    if (userChoice === "...") {
+        document.querySelector("#results").innerHTML = "You lose! Hesitation is death!";
+        computerWinCount++;
+    }
+    else if (userChoice === computerChoice) {
         document.querySelector("#results").innerHTML = "It's a tie!";
     } else if (userChoice === "Rock" && computerChoice === "Paper") {
         document.querySelector("#results").innerHTML = "You lose! Paper covers Rock!";
@@ -46,22 +76,33 @@ function decideWinner(userChoice, computerChoice) {
 }
 
 function checkGameState() {
-    if (computerWinCount === 5 || userWinCount === 5) {
-        alert("Game Over!");
+    if (computerWinCount === 5) {
+       document.querySelector("#score").innerHTML = "You lose! The computer made it to 5 points first!"
+       document.getElementById("play-again-button").className = "active";
+    } else if (userWinCount === 5) {
+        document.querySelector("#score").innerHTML = "You win! You made it to 5 points first!"
+        document.getElementById("play-again-button").className = "active";
     }
 }
 
+function playAgain() {
+    location.reload();
+    return false;
+}
 
-
+let playButton = document.getElementById("play-game-button");
 let rockButton = document.getElementById("Rock");
 let paperButton = document.getElementById("Paper");
 let scissorsButton = document.getElementById("Scissors");
+let playAgainButton = document.getElementById("play-again-button");
 let userWinCount = 0;
 let computerWinCount = 0;
 
+playButton.onclick = displayGame;
 rockButton.onclick = displayUserChoice;
 paperButton.onclick = displayUserChoice;
 scissorsButton.onclick = displayUserChoice;
+playAgainButton.onclick = playAgain;
 
 document.querySelector("#computer-score").innerHTML = computerWinCount;
 document.querySelector("#user-score").innerHTML = userWinCount;
